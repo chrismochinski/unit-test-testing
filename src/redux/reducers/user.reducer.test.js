18636b4d4ecff7c64377a4c11a108db5ca16d3a2
Chris "Mo" Mochinski <cmochinski@gmail.com>
2021-10-29 16:30:32 -0500

    reducer test reorg

---
+++ src/redux/reducers/user.reducer.test.js
@@ -0,0 +1,13 @@
+import userReducer from './user.reducer.js'; 
+
+describe('User reducer tests', () => { //this is just for you to see in terminal
+    test('The default value is an empty object.', (done) => { // KEEP THIS DESCRIPTIVE AND STRAIGHTFORWARD - not jargon - QA team and MGR will read
+        let action = {};
+        let output = userReducer(undefined, action)
+        expect(typeof output).toBe('object'); //expect will give you a pass or fail
+        // expect(output).toBeDefined();
+        done();
+    }) 
+});
+
+//unsure of what to expect?? 
